907b4e75e6157143ed8dbb80caaec2ebf0d04b68
Abdukodir <Abdukodir_Soliev@epam.com>
2022-03-01 00:50:57 +0500

    add removed library file

---
+++ ConcatetionLogicLibrary/ConcatetionLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConcatetionLogicLibrary
+{
+    public class ConcatetionLogic
+    {
+        private string username;
+        public ConcatetionLogic(string username)
+        {
+            this.username = username;
+        }
+
+        public string Output()
+        {
+            return $"{DateTime.Now} Hello, {username}";
+        }
+    }
+}
